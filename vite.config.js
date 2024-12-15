711af6b8683aca0f611748c03a8cda778988d760
Mahammad <mahammad.code@gmail.com>
2024-12-15 21:58:13 +0400

    vite config removed object

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://164.92.190.92',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  },
   build: {
     outDir: 'build' // Specify the output folder as 'build'
   },
